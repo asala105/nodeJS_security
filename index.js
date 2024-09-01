17d239ca0959b7b9355bacac27a071938f9cd180
Asala El Ghandour <asala@swftbox.com>
2024-09-01 23:58:26 +0300

    adding express-rate-limit

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import routes from './src/routes/crmRoutes.js';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import routes from "./src/routes/crmRoutes.js";
 
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mongo:27017/docker-node-mongo');
+mongoose.connect("mongodb://mongo:27017/docker-node-mongo");
 
-app.use(helmet())
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //time in milliseconds
+  max: 100, //max number of requests per the time in milliseconds
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,12 +26,10 @@ app.use(bodyParser.json());
 routes(app);
 
 // serving static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
-app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`your server is running on port ${PORT}`));
