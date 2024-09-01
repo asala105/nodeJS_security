1c0f249f3bd13bd4d4ed4b5a9dc534dcda3ecb65
Asala El Ghandour <asala@swftbox.com>
2024-09-02 00:16:43 +0300

    using rateLimiter and cookieSession in the application

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
+import cookieSession from "cookie-session";
 import routes from "./src/routes/crmRoutes.js";
 
 const app = express();
@@ -15,9 +16,25 @@ mongoose.connect("mongodb://mongo:27017/docker-node-mongo");
 app.use(helmet());
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, //time in milliseconds
-  max: 100, //max number of requests per the time in milliseconds
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [
+      /* secret keys */
+    ],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
